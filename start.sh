d34badab5b30ed433b2bc4bc74b86012faa26fae
grhre <129239200+grhre@users.noreply.github.com>
2023-03-28 20:27:17 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # set val
 python3 /app/self-ping.py &
 
-AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+AUUID=8bcb468e-b5c8-434c-9294-fc0967236a73
 ParameterSSENCYPT=chacha20-ietf-poly1305
 CADDYIndexPage=https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html
 
